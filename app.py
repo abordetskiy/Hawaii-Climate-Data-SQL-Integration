6f908c9362a3298eb431d590484eca88abb6df71
Artem Bordetskiy <abordetskiy@gmail.com>
2020-09-14 13:22:39 -0500

    feat: add precipitation app route

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # Import Dependencies
 from flask import Flask, jsonify
+import pandas as pd
+import json
 # Initialize Flask
 app = Flask(__name__)
 # Flask Routes
@@ -9,19 +11,24 @@ def home():
     print("Home Page Accessed")
     return '''Welcome to the Climate Analysis app page!<br>
     Available Routes:<br>
+    <br>
     /api/v1.0/precipitation<br>
     /api/v1.0/stations<br>
     /api/v1.0/tobs<br>
     /api/v1.0/<start><br>
     /api/v1.0/<start>/<end><br>
-    
-    Please enter all dates in a %Y-%m-%d (YYYY-MM-DD) format
+    <br>
+    NOTE: Please enter all dates in a %Y-%m-%d (YYYY-MM-DD) format
     '''
 
 @app.route("/api/v1.0/precipitation")
 def Precipitation():
-    #jsonPrecipitation = jsonify(PLACEHOLDER)
-    jsonPrecipitation = "#"
+    # Read output csv from .ipynb script
+    jsonPrecipitation_df = pd.read_csv("Resources/Last12moPrcp.csv")
+    # Convert dataframe to json format
+    jsonPrecipitation_json = jsonPrecipitation_df.to_json(orient="index")
+    # creates json object capable of being parsed for pretty print
+    jsonPrecipitation = json.loads(jsonPrecipitation_json)
     print("precipitation Page Accessed")
     return jsonPrecipitation
 
